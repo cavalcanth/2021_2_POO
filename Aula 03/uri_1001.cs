01010f369aecd57676a56357a91c6de182d6b0ba
CARLOS CAVALCANTI <>
2021-11-10 17:40:21 +0000

    Adicionando uri_1001

---
+++ Aula 03/uri_1001.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int x = a + b;
+    
+    Console.WriteLine($"x = {x}");
+  }
+}
