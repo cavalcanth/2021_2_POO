e5a7c0cda5ff797f84d5289f18af954ba298cdd9
CARLOS CAVALCANTI <94067454+cavalcanth@users.noreply.github.com>
2021-11-18 22:55:14 +0000

    Soma dos ímpares

---
+++ Aula 06/SomaImpares.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    int s = 0;
+    int n = 1;
+    while (n < 20){
+
+      Console.WriteLine(n);
+      s += n;
+      n += 2;
+
+    }
+    Console.WriteLine($"Soma = {s}");
+  }
+}
