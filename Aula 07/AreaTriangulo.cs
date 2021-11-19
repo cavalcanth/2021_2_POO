b3ad0a65eaf87ce92da2d2abc9d4a3904b48c03b
CARLOS CAVALCANTI <94067454+cavalcanth@users.noreply.github.com>
2021-11-19 12:32:03 +0000

    Aula 07

---
+++ Aula 07/AreaTriangulo.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    /*double x = AreaTriangulo(10, 20);
+    double y = AreaTriangulo(5, 8);
+    Console.WriteLine(x);
+    Console.WriteLine(y);*/
+
+    double x = Area.AreaTriangulo(10, 20);
+    double y = Area.AreaTriangulo(5, 8);
+    Console.WriteLine(x);
+    Console.WriteLine(y);
+
+  }
+
+  /*public static double AreaTriangulo (double b, double h){
+    double area = b * h / 2;
+    return area;
+  }*/
+
+  //Como o método está na mesma classe, pode ser definida por privado
+
+  /*private static double AreaTriangulo (double b, double h){
+    double area = b * h / 2;
+    return area;
+  }*/
+
+  class Area{ //o métoddo deve ser público
+    public static double AreaTriangulo (double b, double h){
+    double area = b * h / 2;
+    return area;
+  }
+  }
+
+}
